1dc60dc9dfcdce74100fb6d29a65b8a0356e417c
markus-strenn <markus.strenn@gmail.com>
2016-12-11 17:23:32 +0100

    added theme

--- src/main/java/red/hero/admin/client/HelloWorld.java
+++ src/main/java/red/hero/admin/client/HelloWorld.java
@@ -1,11 +1,13 @@
 package red.hero.admin.client;
 
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 
+@Theme("valo")
 @SpringUI
 public class HelloWorld extends UI {
 
